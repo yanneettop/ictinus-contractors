921bf0e8c448f4fa75d23d40482ac6cec1d10ea0
yanneettop <chesterlp0@gmail.com>
2026-07-20 01:43:43 +0100

    Generate direct Assistant route

--- scripts/generate-route-html.mjs
+++ scripts/generate-route-html.mjs
@@ -24,6 +24,7 @@ const routes = [
   '/job-manager/login',
   '/job-manager/update-password',
   '/job-manager/calendar',
+  '/job-manager/assistant',
   '/job-manager/projects',
   '/job-manager/files',
   '/job-manager/projects/new',
